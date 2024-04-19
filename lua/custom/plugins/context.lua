9ca7cdb6b055cfb0e38c65db039dcade963e78b0
HYDRAoo7 <pandyavedant6565@gmail.com>
2024-04-19 15:52:55 +0530

    added treesiter-context

--- lua/custom/plugins/context.lua
+++ lua/custom/plugins/context.lua
@@ -5,5 +5,8 @@ return {
     vim.keymap.set('n', '[c', function()
       require('treesitter-context').go_to_context()
     end, { silent = true })
+    vim.keymap.set('n', '<leader>tc', function()
+      require('treesitter-context').toggle()
+    end, { silent = true })
   end,
 }
--- lua/vedant/set.lua
+++ lua/vedant/set.lua
@@ -1,28 +1,29 @@
-vim.g.mapleader = " "
-vim.g.maplocalleader = " "
+vim.g.mapleader = ' '
+vim.g.maplocalleader = ' '
 vim.g.have_nerd_font = false
 vim.g.netrw_browse_split = 0
 vim.g.netrw_banner = 0
 vim.g.netrw_winsize = 25
 
 vim.opt.number = true
+vim.opt.smartindent = true
 vim.opt.relativenumber = true
-vim.opt.mouse = "a"
+vim.opt.mouse = 'a'
 vim.opt.showmode = false
-vim.opt.clipboard = "unnamedplus"
+vim.opt.clipboard = 'unnamedplus'
 vim.opt.breakindent = true
 vim.opt.ignorecase = true
 vim.opt.smartcase = true
-vim.opt.signcolumn = "yes"
+vim.opt.signcolumn = 'yes'
 vim.opt.updatetime = 250
 vim.opt.timeoutlen = 300
 vim.opt.splitright = true
 vim.opt.splitbelow = true
 vim.opt.list = true
-vim.opt.listchars = { tab = "» ", trail = "·", nbsp = "␣" }
-vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
+vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
+vim.opt.undodir = os.getenv 'HOME' .. '/.vim/undodir'
 vim.opt.undofile = true
-vim.opt.inccommand = "split"
+vim.opt.inccommand = 'split'
 vim.opt.cursorline = true
 vim.opt.scrolloff = 10
 vim.opt.wrap = true
