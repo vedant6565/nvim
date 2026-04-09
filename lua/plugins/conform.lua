5622b60a10177bdabbec6691de180090ff26bedc
HYDRAoo7 <pandyavedant6565@gmail.com>
2026-04-09 08:48:48 +0530

    feat

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -1,36 +1,47 @@
 return {
-	"stevearc/conform.nvim",
-	event = { "BufWritePre" },
-	cmd = { "ConformInfo" },
-	keys = {
-		{
-			"<leader>f",
-			function()
-				require("conform").format({ async = true, lsp_format = "fallback" })
-			end,
-			mode = "",
-			desc = "[F]ormat buffer",
-		},
-	},
-	---@module 'conform'
-	---@type conform.setupOpts
-	opts = {
-		notify_on_error = false,
-		format_on_save = function(bufnr)
-			local disable_filetypes = { c = true, cpp = true }
-			if disable_filetypes[vim.bo[bufnr].filetype] then
-				return nil
-			else
-				return {
-					timeout_ms = 500,
-					lsp_format = "fallback",
-				}
-			end
-		end,
-		formatters_by_ft = {
-			lua = { "stylua" },
-			javascript = { "prettierd", "prettier", stop_after_first = true },
-			typecript = { "prettierd", "prettier", stop_after_first = true },
-		},
-	},
+  'stevearc/conform.nvim',
+  event = { 'BufWritePre' },
+  cmd = { 'ConformInfo' },
+  keys = {
+    {
+      '<leader>f',
+      function()
+        require('conform').format { async = true, lsp_format = 'fallback' }
+      end,
+      mode = '',
+      desc = '[F]ormat buffer',
+    },
+  },
+  ---@module 'conform'
+  ---@type conform.setupOpts
+  opts = {
+    notify_on_error = false,
+    format_on_save = function(bufnr)
+      local disable_filetypes = { c = true, cpp = true }
+      if disable_filetypes[vim.bo[bufnr].filetype] then
+        return nil
+      else
+        return {
+          timeout_ms = 500,
+          lsp_format = 'fallback',
+        }
+      end
+    end,
+    formatters_by_ft = {
+      lua = { 'stylua' },
+      cpp = { 'clang-format' },
+      c = { 'clang-format' },
+      javascript = { 'biome', 'prettierd', 'prettier', stop_after_first = true },
+      javascriptreact = { 'prettierd', 'prettier', stop_after_first = true },
+      typescript = { 'prettierd', 'prettier', stop_after_first = true },
+      typescriptreact = { 'prettierd', 'prettier', stop_after_first = true },
+      html = { 'prettierd', 'prettier', stop_after_first = true },
+      css = { 'prettierd', 'prettier', stop_after_first = true },
+      json = { 'prettierd', 'prettier', stop_after_first = true },
+      jsonc = { 'prettierd', 'prettier', stop_after_first = true },
+      go = { 'goimports_reviser', 'gofumpt' },
+      http = { 'kulala-fmt' },
+      bash = { 'beautysh' },
+    },
+  },
 }
